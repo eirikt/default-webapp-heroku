1f5f7e824f10ca3f47f7067c9ba7ffc098838ee4
Eirik Torske <eirik.torske@gmail.com>
2015-08-14 08:37:47 +0200

    v0.2.0: Vanilla Node.js 'Hello World'

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var http = require('http'),
     port = process.env.PORT || 8000,
     server = http.createServer(function(request, response) {
-        response.writeHead(200);
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<html>');
+        response.write('<head>');
+        response.write('<title>Hello World</title>');
+        response.write('</head>');
+        response.write('<body>');
+        response.write('Hello World!');
+        response.write('</body>');
+        response.write('</html>');
         response.end();
     });
 
