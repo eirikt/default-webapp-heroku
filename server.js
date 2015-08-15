180f48f8b3e927335092bbd409175ba64b623fa4
Eirik Torske <eirik.torske@gmail.com>
2015-08-15 09:49:36 +0200

    (Empty) Favicon trick to avoid 404 in browsers

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var path = require('path'),
 appServer = express();
 appServer.use(express.static(path.join(applicationAbsolutePath, clientResourcesRelativePath)));
 
+// (Empty) Favicon trick to avoid '404 Not Found' in browsers
+appServer.get('/favicon.ico', function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'image/x-icon'
+    });
+    response.end();
+});
+
 // Start application server
 appServer.listen(port, function () {
     console.log('Application server listening at port %s', port);
