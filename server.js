abcdfcab93961dceff8d29a1faae49a798313d42
Eirik Torske <eirik.torske@gmail.com>
2015-08-16 13:15:09 +0200

    HTTP Header settings suggesting no caching whatsoever

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var path = require('path'),
 
 // Application server configuration
 appServer = express();
+// Default: Header settings suggesting no caching whatsoever
+appServer.use(function(request, response, next) {
+    response.setHeader("Pragma", "no-cache"); // HTTP 1.0
+    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+    response.setHeader("Expires", "0"); // Proxies
+    return next();
+});
 appServer.use(express.static(path.join(applicationAbsolutePath, clientResourcesRelativePath)));
 
 // (Empty) Favicon trick to avoid '404 Not Found' in browsers
